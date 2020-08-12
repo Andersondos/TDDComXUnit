e100ce664bb7c20f463f703f09358298e96d06d7
adoliveira@vectoritcgroup.com-at-021171016440 <adoliveira@vectoritcgroup.com>
2020-08-12 19:19:31 -0300

    refatorando

--- test/TddXUnit.DominioTest/Curso/CursoTest.cs
+++ test/TddXUnit.DominioTest/Curso/CursoTest.cs
@@ -28,6 +28,25 @@ namespace TddXUnit.DominioTest.Curso
             // cursoEsperado.ToExpectedObject().ShoulMatch(curso);
 
         }
+
+        [Fact]
+        public void NaoDeveCursoUmNomeVazio()
+        {
+              var cursoEsperado = new 
+            {
+                Nome = "Informatica básica",
+                CargaHoraria = 80,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                Valor = 950
+            };
+
+            
+            var curso = new Curso( cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+            
+
+            Assert.Throws<ArgumentException>(()=> 
+                new Curso( string.Empty, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor));
+        }
     }
 
     public enum PublicoAlvo
@@ -42,6 +61,11 @@ namespace TddXUnit.DominioTest.Curso
     {
         public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, double valor)
         {
+            if(nome == string.Empty)
+            {
+                throw new ArgumentException();
+            }
+
             Nome = nome;
             CargaHoraria = cargaHoraria;
             PublicoAlvo = publicoAlvo;

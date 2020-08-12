16943f8de4ee4d1681fb7a6ac50cf7e89815ab53
andersondos <anderson.d.o.s.two@gmailcom>
2020-08-12 14:55:50 -0300

    criando projeto e o primeiro teste de curso

---
+++ test/TddXUnit.DominioTest/Curso/CursoTest.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+
+
+namespace TddXUnit.DominioTest.Curso
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            var cursoEsperado = new 
+            {
+                Nome = "Informatica básica",
+                CargaHoraria = 80,
+                PublicoAlvo = "Estudante",
+                Valor = 950
+            };
+
+            
+            var curso = new Curso( cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+            
+            Assert.Equal(cursoEsperado.Nome, curso.Nome);
+            Assert.Equal(cursoEsperado.CargaHoraria, curso.CargaHoraria);
+            Assert.Equal(cursoEsperado.PublicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(cursoEsperado.Valor, curso.Valor);
+
+            // cursoEsperado.ToExpectedObject().ShoulMatch(curso);
+
+        }
+    }
+
+    public class Curso
+    {
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        {
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+
+        }
+
+        public string Nome { get; private set; }
+        public double CargaHoraria {get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public double Valor {get; private set; }
+    }
+}
